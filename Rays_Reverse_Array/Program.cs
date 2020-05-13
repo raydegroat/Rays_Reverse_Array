eb785cabf5aba8d65d51c4334330b3f1a1b83de4
raydegroat <stockholmray@gmail.com>
2020-05-13 16:25:40 +0200

    first commit

---
+++ Rays_Reverse_Array/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Rays_Reverse_Array
+{
+    class Program
+    {
+
+        static int getInt()
+        {
+            int result = 0;
+            string answer = Console.ReadLine();
+
+            Int32.TryParse(answer, out result);
+
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+
+            int[] a = new int[5]; // Create an array of int and initialize it
+
+            for(int i = 0; i < a.Length; i++)
+            {
+                Console.Write("Enter integer " + (i+1) + " of " + a.Length + ": ");
+                a[i] = getInt();
+            }
+
+            for(int i = (a.Length - 1); i >= 0; i--)
+            {
+                Console.WriteLine(a[i]);
+            }
+        }
+    }
+}
